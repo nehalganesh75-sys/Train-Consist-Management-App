e174388ea7861aeb08e846a2de7f94b3407ac1c8
nehalganesh75-sys <nehalganesh75>
2026-03-23 09:58:09 +0530

    UC4:Maintain Ordered Bogie IDs

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -1,18 +1,26 @@
-import java.util.HashSet;
+import java.util.LinkedList;
 
 public class TrainConsistApp {
     public static void main(String[] args) {
 
-        // Create HashSet
-        HashSet<String> bogieIDs = new HashSet<>();
+        // Create LinkedList
+        LinkedList<String> train = new LinkedList<>();
 
-        // Add bogie IDs
-        bogieIDs.add("B1");
-        bogieIDs.add("B2");
-        bogieIDs.add("B3");
-        bogieIDs.add("B2"); // Duplicate (ignored)
+        // Add bogies
+        train.add("Engine");
+        train.add("Sleeper");
+        train.add("AC");
+        train.add("Cargo");
+        train.add("Guard");
 
-        // Display unique bogies
-        System.out.println("Unique Bogie IDs: " + bogieIDs);
+        // Insert Pantry Car at position 2
+        train.add(2, "Pantry Car");
+
+        // Remove first and last bogie
+        train.removeFirst();
+        train.removeLast();
+
+        // Display final train consist
+        System.out.println("Final Train Consist: " + train);
     }
 }
